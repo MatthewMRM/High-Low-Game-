ac04b135352866ecb35717f3c8988e8b97616a14
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-18 10:05:31 -0500

    Adding arguments to the High Low game

--- High_Low_Game.php
+++ High_Low_Game.php
@@ -1,17 +1,22 @@
 <?php
-// The specs for the game are:
+
 // game picks a random number between 1 and 100.
 // prompts user to guess the number
 do {
   $randNum = rand(1, 100);
   $guesses = 0;
   $playerGuess = -1;
+
+  if ($argv == 3) {
+    echo "Input a number between {$argv[min(1)]} and {$argv[max(100)]}\n";
+  }
   while ($playerGuess != $randNum) {
     $guesses ++;
     echo "Input a number between 1 and 100.\n";
     $playerGuess = fgets(STDIN);
+
     if ($playerGuess == $randNum) {
-      echo "GOOD GUESS\n";
+      echo "Finally...\n";
       echo "It took you $guesses guesses!\n";
     } else if ($playerGuess < $randNum) {
       echo "HIGHER\n";
@@ -19,6 +24,7 @@ do {
       echo "LOWER\n";
     }
   }
+// Prompts new game  
   echo "New Game?";
   $newGame = trim(fgets(STDIN));
 } while ($newGame == "yes");
